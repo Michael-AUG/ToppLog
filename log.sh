42089ba452f02dde1574e00591cba88f28b79e34
Michael GM5AUG <gm5aug@topple.scot>
2023-08-31 17:02:49 +0100

    update to correct error in CAT frequency, also using '-ne' operators in the IF statements

--- log.sh
+++ log.sh
@@ -11,14 +11,16 @@ echo "CAT control = 1, manual = 0"
 read catcont
 echo "Contest = 1, non contest = 0"
 read contest
+
 if [ "$contest"  == 1 ]
 then
   echo "Callsign,Frequency,Mode,TX-RST,TX-SER,RX-RST,RX-SER,UTC-On,UTC-Off,Operator,Station,Date,Locator" >> contest_log_${station//\//-}$date.csv
   declare -i txser=1
-elif [ -z "$contest" ]
+elif [ "$contest" -ne 1 ]
 then
   echo "Callsign,Frequency,Mode,TX-RST,RX-RST,UTC-On,UTC-Off,Operator,Station,Date,Locator" >> log_${station//\//-}$date.csv
 fi
+
 declare -i y=0
 
 while true
@@ -27,19 +29,22 @@ echo "-----------------------------"
 echo "Callsign"
 read call
 utcon=$(date -u +%H%M%S)
+
 if [ "$catcont" == 1 ]
 then
-  freq=$(rigctl -m2 \get_freq)
+  rawFreq=$(rigctl -m2 \get_freq)
+  freq=$(echo "scale=4; $rawFreq / 1000000" | bc)
   mode=$(rigctl -m2 \get_mode|awk 'NR==1{print $1}')
   echo "Freq and mode from radio"
   echo "$freq $mode"
-elif [ -z "$catcont" ]
+elif [ "$catcont" -ne 1 ]
 then
   echo "Same Freq and Mode = 1 Diff Freq and Mode = 0"
   read same
-  if [ -z "$same" ]
+
+  if [ "$same" -ne 1 ]
   then
-    echo "Frequency"
+    echo "Frequency in MHz"
     read freq
     echo "Mode"
     read mode
@@ -49,6 +54,7 @@ then
     echo "$freq $mode"
   fi
 fi
+
 if [ "$contest" == 1 ]
 then
   declare -i tx=599
@@ -56,32 +62,38 @@ then
   echo "TX Serial $txser"
   echo "RX Serial"
   read rxser
-elif [ -z "$contest" ]
+elif [ "$contest" -ne 1 ]
 then
   echo "TX RST"
   read tx
   echo "RX RST"
   read rx
 fi
+
 utcoff=$(date -u +%H%M%S)
 echo "Correct? 1 or 0"
 echo "$call $freq $mode $tx $rx $utc $op"
 read confirm
+
 if [ "$confirm" == 1 ]
 then
+
   if [ "$contest" == 1 ]
   then
     echo "$call,$freq,$mode,$tx,$txser,$rx,$rxser,$utcon,$utcoff,$op,$station,$date,$locator" >> contest_log_${station//\//-}$date.csv
     txser=$((txser+1))
-  elif [ -z "$contest" ]
+  elif [ "$contest" -ne 1 ]
   then
     echo "$call,$freq,$mode,$tx,$rx,$utcon,$utcoff,$op,$station,$date,$locator" >> log_${station//\//-}$date.csv
   fi
+
   y=$((y+1))
   echo "$y Logged"
-elif [ -z "$confirm" ]
+elif [ "$confirm" -ne 1 ]
 then
   echo "re-enter details"
 fi
+
 sleep 2
+
 done
