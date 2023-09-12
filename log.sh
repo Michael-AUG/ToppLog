527651d32f04f9c6263b22b0251fa0acb02d36d4
Michael GM5AUG <gm5aug@topple.scot>
2023-09-12 15:18:24 +0100

    Updated power level input including with CAT control

--- log.sh
+++ log.sh
@@ -8,20 +8,24 @@ read locator
 date=$(date +%Y%m%d)
 echo "Station Operator"
 read op
-echo "TXCVR Power (W)"
-read power
 echo "CAT control = 1, manual = 0"
 read catcont
+if [ "$catcont" -eq 1 ]
+then
+echo "Maximum TXCVR Power (W)"
+echo "(Here enter the maximum power the radio can provide)"
+read maxPower
+fi
 echo "Contest = 1, non contest = 0"
 read contest
 
 if [ "$contest"  -eq 1 ]
 then
-  echo "Callsign,Frequency,Mode,TX-RST,TX-SER,RX-RST,RX-SER,UTC-On,UTC-Off,Operator,Station,Date,Power,Locator" >> contest_log_$filename_$date.csv
+  echo "Callsign,Frequency,Mode,TX-RST,TX-SER,RX-RST,RX-SER,UTC-On,UTC-Off,Operator,Station,Date,Power,Locator" >> contest_log_$filename\_$date.csv
   declare -i txser=1
 elif [ "$contest" -ne 1 ]
 then
-  echo "Callsign,Frequency,Mode,TX-RST,RX-RST,UTC-On,UTC-Off,Operator,Station,Date,Power,Locator" >> log_$filename_$date.csv
+  echo "Callsign,Frequency,Mode,TX-RST,RX-RST,UTC-On,UTC-Off,Operator,Station,Date,Power,Locator" >> log_$filename\_$date.csv
 fi
 
 declare -i y=0
@@ -38,8 +42,10 @@ then
   rawFreq=$(rigctl -m2 \get_freq)
   freq=$(echo "scale=4; $rawFreq / 1000000" | bc)
   mode=$(rigctl -m2 \get_mode|awk 'NR==1{print $1}')
-  echo "Freq and mode from radio"
-  echo "$freq $mode"
+  tempPower=$(rigctl -m2 \get_level 'RFPOWER')
+  power=$(echo "scale=1; ($maxPower * $tempPower * 100) / 100" | bc)
+  echo "Freq, mode and power from radio"
+  echo "$freq $mode $power"
 elif [ "$catcont" -ne 1 ]
 then
   echo "Same Freq and Mode = 1 Diff Freq and Mode = 0"
@@ -51,10 +57,12 @@ then
     read freq
     echo "Mode"
     read mode
+    echo "Power (in W)"
+    read power
   elif [ "$same" -eq 1 ]
   then
-    echo "Freq and Mode"
-    echo "$freq $mode"
+    echo "Freq, Mode and Power"
+    echo "$freq $mode $power"
   fi
 fi
 
@@ -83,11 +91,11 @@ then
 
   if [ "$contest" -eq 1 ]
   then
-    echo "$call,$freq,$mode,$tx,$txser,$rx,$rxser,$utcon,$utcoff,$op,$station,$date,$power,$locator" >> contest_log_$filename_$date.csv
+    echo "$call,$freq,$mode,$tx,$txser,$rx,$rxser,$utcon,$utcoff,$op,$station,$date,$power,$locator" >> contest_log_$filename\_$date.csv
     txser=$((txser+1))
   elif [ "$contest" -ne 1 ]
   then
-    echo "$call,$freq,$mode,$tx,$rx,$utcon,$utcoff,$op,$station,$date,$power,$locator" >> log_$filename_$date.csv
+    echo "$call,$freq,$mode,$tx,$rx,$utcon,$utcoff,$op,$station,$date,$power,$locator" >> log_$filename\_$date.csv
   fi
 
   y=$((y+1))
